e9014c1e695acff2983f5a8358dc3a0204dcb0ad
Jorge Ronald Cribb <jorge.cribb@litoral-gas.com.ar>
2022-12-05 16:27:39 -0300

    Trying to remove workflows

--- gh-sync-repos.js
+++ gh-sync-repos.js
@@ -1,5 +1,44 @@
 const { exec } = require("child_process");
 
+const removeWorkflows = (user_, repo_) => {
+    exec(
+        `gh run list -R https://github.com/${user_}/${repo_}`,
+        (error, stdout, stderr) => {
+            const list = stdout;
+            list.split("\n").forEach((fila) => {
+                let col = -1;
+                let id = 0;
+                if (fila.length > 0) {
+                    for (i = 0; i <= 5; i++) {
+                        col = fila.indexOf("\t", col + 1);
+                    }
+                    id = fila.substring(col + 1, col + 11);
+                    //console.log(`Workflow ID: ${id}`);
+                    exec(
+                        `gh api repos/${user_}/${repo_}/actions/runs/${id} -X DELETE `,
+                        (error, stdout, stderr) => {
+                            // if (error) {
+                            //     console.error(
+                            //         " Error:",
+                            //         `Eliminación fallida: ${error.message}`
+                            //     );
+                            // } else {
+                            //     console.log(" Status:", " Eliminado");
+                            // }
+                        }
+                    );
+                }
+            });
+            // if (error) {
+            //     console.error(
+            //         " Error:",
+            //         `Sincronización fallida: ${error.message}`
+            //     );
+            // }
+        }
+    );
+};
+
 const syncRepos = async () => {
     console.log("Actualizando");
     let ghUser = "jrcribb";
@@ -21,6 +60,7 @@ const syncRepos = async () => {
                                 (error, stdout, stderr) => {
                                     console.log("REPOSITORY NAME: ",html_url);
                                     console.log("Description: ",description ? description : "Sin descripción");
+                                    removeWorkflows(ghUser, name);
                                     if (error) {
                                         console.error(" Error:",`Sincronización fallida: ${error.message}`);
                                     } else {
