6b61869d9fa6cf3be428783afd4d6b07dedb4edb
jrcribb <jorge.crribb@gmail.com>
2023-03-12 13:13:13 -0300

    Versión 2

--- gh-sync-repos.js
+++ gh-sync-repos.js
@@ -1,40 +1,31 @@
 const { exec } = require("child_process");
 
 const removeWorkflows = (user_, repo_) => {
-    exec(
-        `gh run list -R https://github.com/${user_}/${repo_}`,
+    exec(`gh workflow -R https://github.com/${user_}/${repo_} list`,
         (error, stdout, stderr) => {
             const list = stdout;
             list.split("\n").forEach((fila) => {
                 let col = -1;
                 let id = 0;
                 if (fila.length > 0) {
-                    for (i = 0; i <= 5; i++) {
-                        col = fila.indexOf("\t", col + 1);
-                    }
-                    id = fila.substring(col + 1, col + 11);
-                    //console.log(`Workflow ID: ${id}`);
-                    exec(
-                        `gh api repos/${user_}/${repo_}/actions/runs/${id} -X DELETE `,
+                    id = fila.substring(fila.length - 8);
+                    exec(`gh workflow -R https://github.com/${user_}/${repo_} disable ${id}`,
                         (error, stdout, stderr) => {
-                            // if (error) {
-                            //     console.error(
-                            //         " Error:",
-                            //         `Eliminación fallida: ${error.message}`
-                            //     );
-                            // } else {
-                            //     console.log(" Status:", " Eliminado");
-                            // }
+                            if (error) {
+                                console.error(`Error: Eliminación fallida: ${error.message}`);
+                            } else {
+                                console.log(" Status: Eliminado");
+                            }
                         }
                     );
                 }
             });
-            // if (error) {
-            //     console.error(
-            //         " Error:",
-            //         `Sincronización fallida: ${error.message}`
-            //     );
-            // }
+            if (error) {
+                    console.error(" Error:",`Sincronización fallida: ${error.message}`
+                );
+            } else {
+                console.log(" Status:"," Sincronizado");
+            }
         }
     );
 };
@@ -55,21 +46,20 @@ const syncRepos = async () => {
                     const { html_url, description, name, fork } = rowJson;
                     if (fork) {
                         try {
-                            exec(
-                                `gh repo sync --force ${ghUser}/${name}`,
+                            exec(`gh repo sync --force ${ghUser}/${name}`,
                                 (error, stdout, stderr) => {
                                     console.log("REPOSITORY NAME: ",html_url);
                                     console.log("Description: ",description ? description : "Sin descripción");
-                                    //removeWorkflows(ghUser, name);
+                                    removeWorkflows(ghUser, name);
                                     if (error) {
-                                        console.error(" Error:",`Sincronización fallida: ${error.message}`);
+                                        console.error(`Error: Sincronización fallida: ${error.message}`);
                                     } else {
-                                        console.log(" Status:"," Sincronizado");
+                                        console.log(" Status: Sincronizado");
                                     }
                                 }
                             );
                         } catch (error) {
-                            console.error(" Error:","Error de s.o.");
+                            console.error(" Error: Error de s.o.");
                         }
                     }
                 });
